0bbe320b3546040012f02b27ba2ec582a646d889
Erik Vesterberg <erik.vesterberg@qred.com>
2020-03-13 10:00:40 +0100

    :art: updated with some crud functionality with requests

--- routes/posts.js
+++ routes/posts.js
@@ -2,10 +2,18 @@ const express = require('express');
 const router = express.Router();
 const Post = require('../models/Post')
 
-router.get('/', (req, res) => {
-  res.send('We are on posts');
+
+// Get all posts
+router.get('/', async (req, res) => {
+  try {
+    const posts = await Post.find();
+    res.json(posts);
+  }catch(err) {
+    res.json({message: err})
+  }
 });
 
+// Submit a post
 router.post('/', (req, res) => {
   const post = new Post({
     title: req.body.title,
@@ -16,6 +24,26 @@ router.post('/', (req, res) => {
     res.json(savedPost);
   }
   catch(err) {
+      res.json({ message: err })
+    }
+  })
+
+  //Specific posts
+  router.get('/:postId', async (req, res) => {
+    const post = Post.findById(req.params.postId);
+    try {
+      res.json(post);
+    } catch(err) {
+      res.json({ message: err })
+    }
+  })
+
+  //Delete post
+  router.delete('/:postId', async (req, res) => {
+    try {
+      const deletedPost =  await Post.remove({_id: req.params.postId});
+      res.json(deletedPost);
+    } catch(err) {
       res.json({ message: err })
     }
   })
